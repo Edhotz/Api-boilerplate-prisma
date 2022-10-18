9e6549fec5ef646ca6a51a92b9c319c389cab79c
Edmauro Goma <95288145+EdmauroGoma@users.noreply.github.com>
2022-10-18 11:42:27 +0100

    Estudos da api

---
+++ prisma/migrations/20220930123328_create_category/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "categorys" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categorys_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20220930125353_create_product_category/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `categorys` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "categorys";
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products_categories" (
+    "id" TEXT NOT NULL,
+    "id_product" TEXT NOT NULL,
+    "id_category" TEXT NOT NULL,
+
+    CONSTRAINT "products_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products_categories" ADD CONSTRAINT "products_categories_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_categories" ADD CONSTRAINT "products_categories_id_category_fkey" FOREIGN KEY ("id_category") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
